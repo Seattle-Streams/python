ad49b258e1c0d7405a17040df0ef1a131c228cfb
Tristan Macell <tristanmacelli@gmail.com>
2020-05-23 00:25:43 -0700

    [ADD] double quotes around variables in if statements in deploy script

--- lambda/twilio/deploy.sh
+++ lambda/twilio/deploy.sh
@@ -11,7 +11,7 @@ REGION=${REGION:-uw-west-2}
 # Parsing through input variables
 while [ $# -gt 0 ]; do
 
-   if [ $1 == *"--"* ]; then
+   if [ "$1" == *"--"* ]; then
         param="${1/--/}"
         declare $param="$2"
    fi
@@ -28,7 +28,7 @@ aws lambda update-function-code --function-name $FUNCTION_NAME --s3-bucket $BUCK
 branch_name=$(git symbolic-ref HEAD 2>/dev/null)
 branch_name=${branch_name##refs/heads/}
 
-if [ $branch_name == $branch ]; then
+if [ "$branch_name" == "$branch" ]; then
     echo "Publing new lambda function version"
     lambdaVersion=$(aws lambda publish-version --function-name $FUNCTION_NAME --region $REGION | jq -r '.Version')
     aws lambda update-alias --function-name $FUNCTION_NAME --name production --region $REGION --function-version ${lambdaVersion}      
