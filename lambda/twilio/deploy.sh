83c15d2763ba16bd3a7d7c2de3e2348247afd351
Tristan Macell <tristanmacelli@gmail.com>
2020-05-23 00:21:42 -0700

    [ADD] echo statements to deploy script for readability

--- lambda/twilio/deploy.sh
+++ lambda/twilio/deploy.sh
@@ -11,7 +11,7 @@ REGION=${REGION:-uw-west-2}
 # Parsing through input variables
 while [ $# -gt 0 ]; do
 
-   if [[ $1 == *"--"* ]]; then
+   if [ $1 == *"--"* ]; then
         param="${1/--/}"
         declare $param="$2"
    fi
@@ -19,15 +19,17 @@ while [ $# -gt 0 ]; do
   shift
 done
 
+echo "Pushing build to S3"
 aws s3 cp ${commitID}.zip s3://$BUCKET/$APP/
 
+echo "Pushing to build to lambda"
 aws lambda update-function-code --function-name $FUNCTION_NAME --s3-bucket $BUCKET --s3-key $APP/${commitID}.zip --region $REGION
 
 branch_name=$(git symbolic-ref HEAD 2>/dev/null)
 branch_name=${branch_name##refs/heads/}
 
 if [ $branch_name == $branch ]; then
+    echo "Publing new lambda function version"
     lambdaVersion=$(aws lambda publish-version --function-name $FUNCTION_NAME --region $REGION | jq -r '.Version')
-    echo $lambdaVersion
     aws lambda update-alias --function-name $FUNCTION_NAME --name production --region $REGION --function-version ${lambdaVersion}      
 fi
